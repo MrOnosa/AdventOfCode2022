dab9ea2fbbb6f4c8a51d06c37516a81bb0b04dbc
MrOnosa <supersnapple@gmail.com>
2022-11-30 18:00:26 -0600

    Preparation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# AdventOfCode2022
-My answers for Advent of Code 2022
+# AdventOfCode2021
+
+My answers for Advent of Code 2022. This is really hacky code to get these tasks done. They are not indicative of my typical programming style.
---
+++ init.ps1
@@ -0,0 +1,27 @@
+﻿for ($i = 1; $i -le 25; $i += 1)
+{
+    mkdir "Day$i"
+    New-Item "Day$i\test-input.txt"
+    New-Item "Day$i\puzzle-input.txt"
+    mkdir "Day$i\Day$i-1"
+    mkdir "Day$i\Day$i-2"
+    Set-Location "Day$i\Day$i-1"
+    dotnet new console --framework net7.0
+    dotnet new gitignore
+    Remove-Item "Program.cs"
+    Copy-Item "..\..\Template.cs" -Destination "Program.cs"
+    Copy-Item "..\..\.template-vscode" -Recurse -Destination ".vscode"
+    ((Get-Content -path ".\.vscode\launch.json" -Raw) -replace "Day1-1","Day$i-1") | Set-Content -Path ".\.vscode\launch.json"
+    ((Get-Content -path ".\.vscode\tasks.json" -Raw) -replace "Day1-1","Day$i-1") | Set-Content -Path ".\.vscode\tasks.json"
+    dotnet build
+    Set-Location "..\..\Day$i\Day$i-2"
+    dotnet new console --framework net7.0
+    dotnet new gitignore
+    Remove-Item "Program.cs"
+    Copy-Item "..\..\Template.cs" -Destination "Program.cs"
+    Copy-Item "..\..\.template-vscode" -Recurse -Destination ".vscode"
+    ((Get-Content -path ".\.vscode\launch.json" -Raw) -replace "Day1-1","Day$i-2") | Set-Content -Path ".\.vscode\launch.json"
+    ((Get-Content -path ".\.vscode\tasks.json" -Raw) -replace "Day1-1","Day$i-2") | Set-Content -Path ".\.vscode\tasks.json"
+    dotnet build
+    Set-Location "..\..\"
+}
