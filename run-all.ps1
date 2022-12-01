0a4b34f5dbe576a769e80d1d9b1bd9526a756f75
MrOnosa <supersnapple@gmail.com>
2022-11-30 19:01:53 -0600

    Preparation

--- run-all.ps1
+++ run-all.ps1
@@ -1,14 +1,14 @@
-﻿echo "This wasn't as interesting as I thought it would be since part of the time captured is the dotnet run command itself which takes nearly a second"
+﻿echo "This wasn't as interesting as I thought it would be since part of the time captured is the dotnet run command itself which takes nearly a second. With no answers, this took 00:01:03.2730547."
 $fullStopwatch =  [system.diagnostics.stopwatch]::StartNew()
 for ($i = 1; $i -le 25; $i += 1)
 {
     $stopwatchOne =  [system.diagnostics.stopwatch]::StartNew()
-    cd "Day$i\Day$i-1"
+    cd ("Day{0:D2}\Day{0:D2}-1" -f $i)
     dotnet run | Out-Null
     $stopwatchOne.Stop()
     $r1 = $stopwatchOne.Elapsed.ToString('hh\:mm\:ss\.fffffff')
     echo "Day $i-1 - $r1"
-    cd "..\..\Day$i\Day$i-2"
+    cd ("..\..\Day{0:D2}\Day{0:D2}-2" -f $i)
     $stopwatchTwo =  [system.diagnostics.stopwatch]::StartNew()
     dotnet run | Out-Null
     cd "..\..\"
