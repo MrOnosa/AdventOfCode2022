22040abaa085883c6756124a89ad505a2dac01d6
MrOnosa <supersnapple@gmail.com>
2022-12-01 05:32:19 -0600

    Day 1

--- Day01/Day01-1/Program.cs
+++ Day01/Day01-1/Program.cs
@@ -1,13 +1,34 @@
 ﻿using System.Diagnostics;
 
 Console.WriteLine("Hello World!");
-foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@".\..\test-input.txt"))
-{
-    Console.WriteLine($"Input: {line}");
-}
 Stopwatch stopWatch = new Stopwatch();
 stopWatch.Start();
+List<int> calorieAggregation = new List<int>();
+int elfIndex = 0;
+foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@".\..\puzzle-input.txt"))
+{
+    Console.WriteLine($"Input: {line}");
+    if(string.IsNullOrWhiteSpace(line)){
+    Console.WriteLine($"Elf #{elfIndex+1} had  {calorieAggregation[elfIndex]} calories.");
+        elfIndex++;
+    }
+    else if(calorieAggregation.Count == elfIndex){
+        calorieAggregation.Add(int.Parse(line));
+    } else {
+        calorieAggregation[elfIndex]+=int.Parse(line);
+    }
+}
+
+int mostIndex=0;
+for (int i = 1; i < calorieAggregation.Count; i++)
+{
+    int currentWinner = calorieAggregation[mostIndex];
+    int calories = calorieAggregation[i];
+    if(calories > currentWinner){
+        mostIndex = i;
+    }
+}
 
 stopWatch.Stop();
 
-Console.WriteLine($"Result: - Elapsed {stopWatch.Elapsed} ");
+Console.WriteLine($"Result: Elf #{mostIndex+1} had the most calories with {calorieAggregation[mostIndex]} - Elapsed {stopWatch.Elapsed} ");
--- Day01/Day01-2/Program.cs
+++ Day01/Day01-2/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System.Diagnostics;
 
 Console.WriteLine("Hello World!");
-foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@".\..\test-input.txt"))
-{
-    Console.WriteLine($"Input: {line}");
-}
 Stopwatch stopWatch = new Stopwatch();
 stopWatch.Start();
+List<int> calorieAggregation = new List<int>();
+int elfIndex = 0;
+foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@".\..\puzzle-input.txt"))
+{
+    Console.WriteLine($"Input: {line}");
+    if(string.IsNullOrWhiteSpace(line)){
+    Console.WriteLine($"Elf #{elfIndex+1} had  {calorieAggregation[elfIndex]} calories.");
+        elfIndex++;
+    }
+    else if(calorieAggregation.Count == elfIndex){
+        calorieAggregation.Add(int.Parse(line));
+    } else {
+        calorieAggregation[elfIndex]+=int.Parse(line);
+    }
+}
+calorieAggregation = calorieAggregation.OrderByDescending(c => c).ToList();
 
 stopWatch.Stop();
 
-Console.WriteLine($"Result: - Elapsed {stopWatch.Elapsed} ");
+Console.WriteLine($"Result: {calorieAggregation.Take(3).Sum()} - Elapsed {stopWatch.Elapsed} ");
