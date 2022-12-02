5048052105a531ce5ad8f54f4a3937e5f883626d
MrOnosa <supersnapple@gmail.com>
2022-12-02 05:45:23 -0600

    Day 2

--- Day02/Day02-1/Program.cs
+++ Day02/Day02-1/Program.cs
@@ -1,17 +1,61 @@
 using System.Diagnostics;
 
 Console.WriteLine("Day 2-1");
-foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"test-input.txt"))
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+var rounds = new List<Round>();
+foreach (var line /*Store text into string records*/ in File.ReadLines(@"puzzle-input.txt"))
 {
     Console.WriteLine($"Input: {line}");
+    rounds.Add(new Round
+    {
+        ElfMove = line[0],
+        YourMove = line[2]
+    });
 }
-Stopwatch stopWatch = new Stopwatch();
-stopWatch.Start();
+
+var sum = 0;
+foreach (var round in rounds) sum += round.Outcome();
 
 stopWatch.Stop();
 
-Console.WriteLine($"Result: - Elapsed {stopWatch.Elapsed} ");
+Console.WriteLine($"Result: {sum} - Elapsed {stopWatch.Elapsed} ");
 
+internal struct Round
+{
+    public const int Loss = 0;
+    public const int Tie = 3;
+    public const int Win = 6;
+    public const int Rock = 1;
+    public const int Paper = 2;
+    public const int Scissors = 3;
 
+    public char ElfMove;
+    public char YourMove;
 
+    public int Outcome()
+    {
+        if (ElfMove == 'A') //Rock
+        {
+            if (YourMove == 'X') return Tie + Rock;
+            if (YourMove == 'Y') return Win + Paper;
+            if (YourMove == 'Z') return Loss + Scissors;
+        }
 
+        if (ElfMove == 'B') //Paper
+        {
+            if (YourMove == 'X') return Loss + Rock;
+            if (YourMove == 'Y') return Tie + Paper;
+            if (YourMove == 'Z') return Win + Scissors;
+        }
+
+        if (ElfMove == 'C') //Scissors
+        {
+            if (YourMove == 'X') return Win + Rock;
+            if (YourMove == 'Y') return Loss + Paper;
+            if (YourMove == 'Z') return Tie + Scissors;
+        }
+
+        throw new Exception(ToString());
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# AdventOfCode2021
+# AdventOfCode2022
 
 My answers for Advent of Code 2022. This is really hacky code to get these tasks done. They are not indicative of my typical programming style.
