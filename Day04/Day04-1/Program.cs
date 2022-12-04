65d3aee118a79b4febff4ee8050307ec8f72231e
MrOnosa <supersnapple@gmail.com>
2022-12-04 07:28:58 -0600

    Day 4

--- Day04/Day04-1/Program.cs
+++ Day04/Day04-1/Program.cs
@@ -1,17 +1,39 @@
 using System.Diagnostics;
 
 Console.WriteLine("Day 4-1");
-foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"test-input.txt"))
+var fullOverlap = 0;
+var assignments = new List<Assignment>();
+foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"puzzle-input.txt"))
 {
     Console.WriteLine($"Input: {line}");
+    var item = new Assignment();
+    var elfs = line.Split(',');
+    item.start1 = int.Parse(elfs[0].Split('-')[0]);
+    item.end1 = int.Parse(elfs[0].Split('-')[1]);
+    item.start2 = int.Parse(elfs[1].Split('-')[0]);
+    item.end2 = int.Parse(elfs[1].Split('-')[1]);
+    assignments.Add(item);
 }
+
 Stopwatch stopWatch = new Stopwatch();
 stopWatch.Start();
+foreach (var assignment in assignments)
+{
+    if ((assignment.start1 <= assignment.start2 && assignment.end1 >= assignment.end2) ||
+        assignment.start2 <= assignment.start1 && assignment.end2 >= assignment.end1)
+    {
+        fullOverlap++;
+    }
+}
 
 stopWatch.Stop();
 
-Console.WriteLine($"Result: - Elapsed {stopWatch.Elapsed} ");
+Console.WriteLine($"Result: {fullOverlap} - Elapsed {stopWatch.Elapsed} ");
 
-
-
-
+internal class Assignment
+{
+    public int end1;
+    public int end2;
+    public int start1;
+    public int start2;
+}
--- Day04/Day04-2/Program.cs
+++ Day04/Day04-2/Program.cs
@@ -1,17 +1,41 @@
 using System.Diagnostics;
 
 Console.WriteLine("Day 4-2");
-foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"test-input.txt"))
+var overlap = 0;
+var assignments = new List<Assignment>();
+foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"puzzle-input.txt"))
 {
     Console.WriteLine($"Input: {line}");
+    var item = new Assignment();
+    var elfs = line.Split(',');
+    item.start1 = int.Parse(elfs[0].Split('-')[0]);
+    item.end1 = int.Parse(elfs[0].Split('-')[1]);
+    item.start2 = int.Parse(elfs[1].Split('-')[0]);
+    item.end2 = int.Parse(elfs[1].Split('-')[1]);
+    assignments.Add(item);
 }
+
 Stopwatch stopWatch = new Stopwatch();
 stopWatch.Start();
+foreach (var a in assignments)
+{
+    if ((a.start1 <= a.start2 && a.end1 >= a.end2) ||
+        (a.start2 <= a.start1 && a.end2 >= a.end1) ||
+        (a.start1 >= a.start2 && a.start1 <= a.end2) ||
+        (a.start2 >= a.start1 && a.start2 <= a.end1))
+    {
+        overlap++;
+    }
+}
 
 stopWatch.Stop();
 
-Console.WriteLine($"Result: - Elapsed {stopWatch.Elapsed} ");
+Console.WriteLine($"Result: {overlap} - Elapsed {stopWatch.Elapsed} ");
 
-
-
-
+internal class Assignment
+{
+    public int end1;
+    public int end2;
+    public int start1;
+    public int start2;
+}
