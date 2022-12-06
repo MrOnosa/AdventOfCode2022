b37ce3de5bc5c9edca496297d53b33d24b40d369
MrOnosa <supersnapple@gmail.com>
2022-12-06 05:13:18 -0600

    Day 6

--- Day06/Day06-1/Program.cs
+++ Day06/Day06-1/Program.cs
@@ -1,17 +1,32 @@
 using System.Diagnostics;
 
 Console.WriteLine("Day 6-1");
-foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"test-input.txt"))
-{
-    Console.WriteLine($"Input: {line}");
-}
+int i = 0;
+
 Stopwatch stopWatch = new Stopwatch();
 stopWatch.Start();
+foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"puzzle-input.txt"))
+{
+    char a = ' ';
+    char b = ' ';
+    char c = ' ';
+    char d = ' ';
+    //Console.WriteLine($"Input: {line}");
+    foreach (var l in line)
+    {
+        i++;
+        a = b;
+        b = c;
+        c = d;
+        d = l;
+        if (i > 4 && a != b && a != c && a != d && b != c && b != d && c != d)
+        {
+            Console.WriteLine($"Result: {i} - {a}{b}{c}{d} ");
+            break;
+        }
+    }
+}
 
 stopWatch.Stop();
 
-Console.WriteLine($"Result: - Elapsed {stopWatch.Elapsed} ");
-
-
-
-
+Console.WriteLine($"Elapsed {stopWatch.Elapsed} ");
--- Day06/Day06-2/Program.cs
+++ Day06/Day06-2/Program.cs
@@ -1,17 +1,47 @@
 using System.Diagnostics;
 
-Console.WriteLine("Day 6-2");
-foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"test-input.txt"))
-{
-    Console.WriteLine($"Input: {line}");
-}
+Console.WriteLine("Day 6-1");
+int i = 0;
+
 Stopwatch stopWatch = new Stopwatch();
 stopWatch.Start();
+foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"puzzle-input.txt"))
+{
+    var stm = new char[14];
+    //Console.WriteLine($"Input: {line}");
+    foreach (var l in line)
+    {
+        i++;
+        for (int j = 0; j < stm.Length - 1; j++)
+        {
+            stm[j] = stm[j + 1];
+        }
+
+        stm[^1] = l;
+        if (i > 14)
+        {
+            bool unique = true;
+            for (int j = 0; j < stm.Length - 1 && unique; j++)
+            {
+                for (int k = j + 1; k < stm.Length && unique; k++)
+                {
+                    if (stm[j] == stm[k])
+                    {
+                        unique = false;
+                        break;
+                    }
+                }
+            }
+
+            if (unique)
+            {
+                Console.WriteLine($"Result: {i} ");
+                break;
+            }
+        }
+    }
+}
 
 stopWatch.Stop();
 
-Console.WriteLine($"Result: - Elapsed {stopWatch.Elapsed} ");
-
-
-
-
+Console.WriteLine($"Elapsed {stopWatch.Elapsed} ");
