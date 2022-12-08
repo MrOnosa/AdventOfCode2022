e28a8790bd7415d4e2f433472d49fb9feec067ff
MrOnosa <supersnapple@gmail.com>
2022-12-08 07:14:10 -0600

    Day 8

--- Day08/Day08-1/Program.cs
+++ Day08/Day08-1/Program.cs
@@ -1,17 +1,100 @@
 using System.Diagnostics;
 
 Console.WriteLine("Day 8-1");
-foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"test-input.txt"))
+var lines = new List<string>();
+foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"puzzle-input.txt"))
 {
-    Console.WriteLine($"Input: {line}");
+    //Console.WriteLine($"Input: {line}");
+    lines.Add(line);
 }
+
 Stopwatch stopWatch = new Stopwatch();
 stopWatch.Start();
 
+int cols = lines[0].Length + 2;
+int rows = lines.Count + 2;
+
+var grid = new int[rows, cols];
+
+for (var r = 0; r < rows; r++)
+{
+    for (int c = 0; c < cols; c++)
+    {
+        if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1)
+        {
+            grid[r, c] = -1;
+        }
+        else
+        {
+            var line = lines[r - 1];
+            grid[r, c] = int.Parse(line[c - 1].ToString());
+        }
+    }
+}
+
+int visible = 0;
+
+for (var r = 1; r < rows - 1; r++)
+{
+    for (int c = 1; c < cols - 1; c++)
+    {
+        var hidden = true;
+
+        //left
+        var left = new List<int>();
+        for (int x = 0; x < c && hidden; x++)
+        {
+            left.Add(grid[r, x]);
+        }
+
+        if (left.All(t => t < grid[r, c]))
+        {
+            visible++;
+            continue;
+        }
+
+        //right
+        var right = new List<int>();
+        for (int x = c + 1; x < cols && hidden; x++)
+        {
+            right.Add(grid[r, x]);
+        }
+
+        if (right.All(t => t < grid[r, c]))
+        {
+            visible++;
+            continue;
+        }
+
+        //up
+        var up = new List<int>();
+        for (int y = 0; y < r && hidden; y++)
+        {
+            up.Add(grid[y, c]);
+        }
+        
+        if (up.All(t => t < grid[r, c]))
+        {
+            visible++;
+            continue;
+        }
+
+        //down
+        var down = new List<int>();
+        for (int y = r + 1; y < rows && hidden; y++)
+        {
+            down.Add(grid[y, c]);
+        }
+        
+        if (down.All(t => t < grid[r, c]))
+        {
+            visible++;
+            continue;
+        }
+    }
+}
+
 stopWatch.Stop();
 
-Console.WriteLine($"Result: - Elapsed {stopWatch.Elapsed} ");
-
-
-
-
+Console.WriteLine($"Result:  - Elapsed {stopWatch.Elapsed} ");
+Console.WriteLine(visible);
--- Day08/Day08-2/Program.cs
+++ Day08/Day08-2/Program.cs
@@ -1,17 +1,127 @@
 using System.Diagnostics;
 
 Console.WriteLine("Day 8-2");
-foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"test-input.txt"))
+var lines = new List<string>();
+foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"puzzle-input.txt"))
 {
-    Console.WriteLine($"Input: {line}");
+    //Console.WriteLine($"Input: {line}");
+    lines.Add(line);
 }
+
 Stopwatch stopWatch = new Stopwatch();
 stopWatch.Start();
 
+int cols = lines[0].Length;
+int rows = lines.Count;
+
+var grid = new int[rows, cols];
+
+for (var r = 0; r < rows; r++)
+{
+    for (int c = 0; c < cols; c++)
+    {
+        var line = lines[r];
+        grid[r, c] = int.Parse(line[c].ToString());
+    }
+}
+
+int visible = 0;
+int highestScore = 0;
+
+for (var r = 0; r < rows; r++)
+{
+    for (int c = 0; c < cols; c++)
+    {
+        int ls = 0;
+        int rs = 0;
+        int us = 0;
+        int ds = 0;
+        //left
+        var left = new Stack<int>();
+        for (int x = 0; x < c; x++)
+        {
+            left.Push(grid[r, x]);
+        }
+
+        while (left.Count > 0)
+        {
+            ls++;
+            if (left.Pop() < grid[r, c])
+            {
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        //right
+        var right = new List<int>();
+        for (int x = c + 1; x < cols; x++)
+        {
+            right.Add(grid[r, x]);
+        }
+
+        foreach (var t in right)
+        {
+            rs++;
+            if (t < grid[r, c])
+            {
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        //up
+        var up = new Stack<int>();
+        for (int y = 0; y < r; y++)
+        {
+            up.Push(grid[y, c]);
+        }
+
+        while (up.Count > 0)
+        {
+            us++;
+            if (up.Pop() < grid[r, c])
+            {
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        //down
+        var down = new List<int>();
+        for (int y = r + 1; y < rows; y++)
+        {
+            down.Add(grid[y, c]);
+        }
+
+        foreach (var t in down)
+        {
+            ds++;
+            if (t < grid[r, c])
+            {
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        int score = ls * rs * us * ds;
+        if (score > highestScore)
+        {
+            highestScore = score;
+            Console.WriteLine($"New high score found at {r},{c} of {highestScore} ");
+        }
+    }
+}
+
 stopWatch.Stop();
 
-Console.WriteLine($"Result: - Elapsed {stopWatch.Elapsed} ");
-
-
-
-
+Console.WriteLine($"Result:  - Elapsed {stopWatch.Elapsed} ");
+Console.WriteLine(highestScore);
