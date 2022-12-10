7eb7703ea6b4158591015b43b12d8d6a5504ed1b
MrOnosa <supersnapple@gmail.com>
2022-12-10 06:03:01 -0600

    Day 10

--- Day10/Day10-1/Program.cs
+++ Day10/Day10-1/Program.cs
@@ -1,17 +1,52 @@
 using System.Diagnostics;
 
 Console.WriteLine("Day 10-1");
-foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"test-input.txt"))
+var lines = new List<string>();
+foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"puzzle-input.txt"))
 {
     Console.WriteLine($"Input: {line}");
+    lines.Add(line);
 }
+
 Stopwatch stopWatch = new Stopwatch();
 stopWatch.Start();
+
+var X = 1;
+int cycle = 0;
+var bits = new List<int>();
+foreach (var line in lines)
+{
+    if (line == "noop")
+    {
+        //during
+        cycle++;
+        storeSignalStrength(cycle, bits, X);
+        //after
+    }
+    else
+    {
+        //first
+        cycle++;
+        storeSignalStrength(cycle, bits, X);
+        //second
+        cycle++;
+        storeSignalStrength(cycle, bits, X);
+        X += int.Parse(line.Split(' ')[1]);
+    }
+
+    if (cycle > 220) break;
+}
 
 stopWatch.Stop();
 
 Console.WriteLine($"Result: - Elapsed {stopWatch.Elapsed} ");
+Console.WriteLine(bits.Sum());
 
-
-
-
+void storeSignalStrength(int cycle, List<int> ints, int X)
+{
+    if ((cycle - 20) % 40 == 0)
+    {
+        ints.Add(cycle * X);
+        Console.WriteLine($"During cycle {cycle}, X is {X}, so signal strength is {ints[^1]}.");
+    }
+}
--- Day10/Day10-2/Program.cs
+++ Day10/Day10-2/Program.cs
@@ -1,17 +1,66 @@
 using System.Diagnostics;
 
 Console.WriteLine("Day 10-2");
-foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"test-input.txt"))
+var lines = new List<string>();
+foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@"puzzle-input.txt"))
 {
     Console.WriteLine($"Input: {line}");
+    lines.Add(line);
 }
+
 Stopwatch stopWatch = new Stopwatch();
 stopWatch.Start();
+
+var X = 1;
+int cycle = 0;
+string hl = string.Empty;
+foreach (var line in lines)
+{
+    if (line == "noop")
+    {
+        //during
+        cycle++;
+        hl += energize(X, cycle);
+        hl = display(hl);
+        //after
+    }
+    else
+    {
+        //first
+        cycle++;
+        hl += energize(X, cycle);
+        hl = display(hl);
+        //second
+        cycle++;
+        hl += energize(X, cycle);
+        hl = display(hl);
+        X += int.Parse(line.Split(' ')[1]);
+    }
+
+    if (cycle > 240) break;
+}
 
 stopWatch.Stop();
 
 Console.WriteLine($"Result: - Elapsed {stopWatch.Elapsed} ");
 
+string display(string line)
+{
+    if (cycle % 40 == 0)
+    {
+        Console.WriteLine(line);
+        return string.Empty;
+    }
 
+    return line;
+}
 
+string energize(int X, int cycle)
+{
+    if (X == cycle % 40 || X + 1 == cycle % 40 || X + 2 == cycle % 40)
+    {
+        return "#";
+    }
 
+    return ".";
+}
